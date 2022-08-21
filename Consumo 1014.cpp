8a0eac606e439ae0fbc05b5e915950dbaa27f4a4
Rute Miliana <rutemiliana13@gmail.com>
2022-08-21 11:42:28 -0300

    add Consumo 1014

---
+++ Consumo 1014.cpp
@@ -0,0 +1,17 @@
+/*
+    https://www.beecrowd.com.br/judge/pt/problems/view/1014
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int X;
+  double Y;
+
+  cin >> X >> Y;
+
+  cout << fixed << setprecision(3) << X / Y << " km/l" << endl;
+
+  return 0;
+} 
